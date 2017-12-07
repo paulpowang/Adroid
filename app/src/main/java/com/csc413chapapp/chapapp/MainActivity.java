5de435bc60b045864a2ba5c418e56d90e07d774c
tommylik <tlik@mail.sfsu.edu>
2017-12-06 20:25:43 -0800

    intial commit

--- app/src/main/java/com/csc413chapapp/chapapp/MainActivity.java
+++ app/src/main/java/com/csc413chapapp/chapapp/MainActivity.java
@@ -2,12 +2,40 @@ package com.csc413chapapp.chapapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.content.Intent;
+
+
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SIGN_IN_REQUEST_CODE = 1;
+    private FirebaseListAdapter<ChatMessage> adapter;
+    RelativeLayout activty_main;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity_main = (RelativeLayout)findViewById(R.id.activity_main);
+
+
+
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+        {
+            startActivityForResult(AuthUI.getInstance[].createSignInIntentBuilder().build[],SIGN_IN_REQUEST_CODE);
+
+
+        }
     }
 }
