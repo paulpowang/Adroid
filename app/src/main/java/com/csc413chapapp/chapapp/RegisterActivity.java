e61180841c07cb6ea3ce38ee71dcfe51c5c808f4
PoHung Wang <wang.po.hung@gmail.com>
2017-12-13 18:26:15 -0800

    activity_login.xml - remove Gmail login button RegisterActivity - add display password checkbox function                    add toast message when user register successful

--- app/src/main/java/com/csc413chapapp/chapapp/RegisterActivity.java
+++ app/src/main/java/com/csc413chapapp/chapapp/RegisterActivity.java
@@ -2,9 +2,14 @@ package com.csc413chapapp.chapapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.*;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,6 +23,10 @@ public class RegisterActivity extends AppCompatActivity{
     private Button submitBttn;
     private EditText txt_email;
     private EditText txt_password;
+
+    //check pw chechbox
+    private EditText pwInput;
+    private CheckBox box;
 
     private FirebaseAuth mAuth;
 
@@ -42,8 +51,29 @@ public class RegisterActivity extends AppCompatActivity{
 
                 mAuth.createUserWithEmailAndPassword(email, password);
 
+                //make Toast message when input username or password invalid
+                Toast.makeText(RegisterActivity.this, "Register Success, Please Return to Login Page to Login", Toast.LENGTH_LONG).show();
+
             }
 
+        });
+
+        //display pw check box function
+        box = (CheckBox) findViewById(R.id.reg_box_pw);
+        pwInput = (EditText) findViewById(R.id.reg_txt_pw);
+        box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    //display password
+                    pwInput.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    //hide password
+                    pwInput.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+                //Keep the cursor at the end of the text
+                pwInput.setSelection(pwInput.getText().length());
+            }
         });
 
     }
