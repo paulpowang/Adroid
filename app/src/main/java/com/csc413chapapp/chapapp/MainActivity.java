198096ce761814f50d4a3325a3240342afa2c9bc
tommylik <tlik@mail.sfsu.edu>
2017-12-15 03:26:44 -0800

    text messages

--- app/src/main/java/com/csc413chapapp/chapapp/MainActivity.java
+++ app/src/main/java/com/csc413chapapp/chapapp/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
