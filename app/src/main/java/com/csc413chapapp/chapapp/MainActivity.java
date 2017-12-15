dc136f048da9b6c77db30ece63cf3a740424417a
arsemab <abeshue@mail.sfsu.edu>
2017-12-15 14:18:22 -0800

    Profile page

--- app/src/main/java/com/csc413chapapp/chapapp/MainActivity.java
+++ app/src/main/java/com/csc413chapapp/chapapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.csc413chapapp.chapapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button start;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button)findViewById(R.id.start);
+       // text = (TextView)findViewById(R.id.old)
+         start.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent neo = new Intent(MainActivity.this, button2.class);
+            startActivity(neo);
+             }
+         });
+
     }
 }
---
+++ app/src/main/java/com/csc413chapapp/chapapp/button2.java
@@ -0,0 +1,9 @@
+package com.csc413chapapp.chapapp;
+
+/**
+ * Created by ArsemaB on 12/15/17.
+ */
+
+public class button2  {
+
+}
