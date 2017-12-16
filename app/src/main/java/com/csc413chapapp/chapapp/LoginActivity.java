0a670b9f4620d0902f850b15905a7250051a0147
tommylik <tlik@mail.sfsu.edu>
2017-12-15 19:10:55 -0800

    gradle fix

--- app/src/main/java/com/csc413chapapp/chapapp/LoginActivity.java
+++ app/src/main/java/com/csc413chapapp/chapapp/LoginActivity.java
@@ -1,30 +1,24 @@
 package com.csc413chapapp.chapapp;
 
 import android.content.DialogInterface;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
-/**
- * Created by Paul on 11/27/2017.
- */
-
 public class LoginActivity extends AppCompatActivity {
-    FirebaseAuth auth;
+
+    FirebaseAuthException auth;
     private String userUID;
-    FirebaseAuth.AuthStateListener authListener;
+    FirebaseAuthException.AuthStateListener authListener;
 
     @Override
     protected void onStart(){
@@ -37,22 +31,23 @@ public class LoginActivity extends AppCompatActivity {
         auth.removeAuthStateListener(authListener);
     }//end of onStop
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        auth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_login2);
+
+        auth = FirebaseAuthException.getInstance();
         //success or error message after login
-        authListener = new FirebaseAuth.AuthStateListener(){
+        authListener = new FirebaseAuthException.AuthStateListener(){
+
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+            public void onAuthStateChanged(@NonNull FirebaseAuthException firebaseAuth){
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user!=null){//Login success
-                    Log.d("onAuthStateChanged", "Login: " + user.getUid());
+                    android.util.Log.d("onAuthStateChanged", "Login: " + user.getUid());
                     userUID = user.getUid();
                 }else{//Logout or Login failed
-                    Log.d("onAuthStateChanged", "Logout");
+                    android.util.Log.d("onAuthStateChanged", "Logout");
                 }
             }
         };//end of authListener
@@ -63,20 +58,20 @@ public class LoginActivity extends AppCompatActivity {
         final String email = ((EditText)findViewById(R.id.login_txt_email)).getText().toString();
         //get the password entered
         final String password = ((EditText)findViewById(R.id.login_txt_pw)).getText().toString();
-        Log.d("AUTH", email+"/"+password);
+        android.util.Log.d("AUTH", email+"/"+password);
         //link to firebase to sign in
         auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                //Login failed
-                if(!task.isSuccessful()){
-                    Log.d("onComplete", "Login failed");
-                    //register new user
-                    register(email,password);
-                }
-            }//end of onComplete
-        });//end of SignIn
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //Login failed
+                        if(!task.isSuccessful()){
+                            android.util.Log.d("onComplete", "Login failed");
+                            //register new user
+                            register(email,password);
+                        }
+                    }//end of onComplete
+                });//end of SignIn
     }//end of login method
 
     private void register (final String email, final String password){
@@ -103,11 +98,11 @@ public class LoginActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 String message;
-                                        if(task.isSuccessful()){
-                                            message = "Register Success!!";
-                                        }else{
-                                            message ="Register Failed!!";
-                                        }
+                                if(task.isSuccessful()){
+                                    message = "Register Success!!";
+                                }else{
+                                    message ="Register Failed!!";
+                                }
                                 new AlertDialog.Builder(LoginActivity.this)
                                         .setMessage(message)
                                         .setPositiveButton("OK", null)
@@ -116,6 +111,7 @@ public class LoginActivity extends AppCompatActivity {
                         });
     }//end of Create New User
 
-
+//
 
 }//end of Login Activity
+
