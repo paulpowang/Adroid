e2a8d830ef3a472d4c9adb82b0b228614fb3ec94
PoHung Wang <wang.po.hung@gmail.com>
2017-12-15 20:17:19 -0800

    add firebase database feature: when new user register, database will store username, email under their Uid under UserInfo directory.
    activity_register.xml: add username text
    RegisterActivity: after user register, update database, return to login page.

--- app/src/main/java/com/csc413chapapp/chapapp/RegisterActivity.java
+++ app/src/main/java/com/csc413chapapp/chapapp/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.csc413chapapp.chapapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.HideReturnsTransformationMethod;
@@ -13,6 +14,9 @@ import android.widget.Toast;
 
 import com.google.firebase.*;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by Paul on 12/5/2017.
@@ -23,12 +27,19 @@ public class RegisterActivity extends AppCompatActivity{
     private Button submitBttn;
     private EditText txt_email;
     private EditText txt_password;
+    private EditText txt_username;
     private CheckBox box;
 
     private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    FirebaseUser user;
 
     String email;
     String password;
+    String username;
+
+    String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -38,6 +49,7 @@ public class RegisterActivity extends AppCompatActivity{
         submitBttn = (Button) findViewById(R.id.reg_btn_submit);
         txt_email = (EditText) findViewById(R.id.reg_txt_email);
         txt_password = (EditText) findViewById(R.id.reg_txt_pw);
+        txt_username = (EditText) findViewById(R.id.reg_username);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -45,12 +57,28 @@ public class RegisterActivity extends AppCompatActivity{
             public void onClick(View v){
                 email = txt_email.getText().toString().trim();
                 password = txt_password.getText().toString().trim();
+                username = txt_username.getText().toString().trim();
 
                 mAuth.createUserWithEmailAndPassword(email, password);
+                uid = mAuth.getCurrentUser().getUid();
+
+                // Write a message to the database
+                database = FirebaseDatabase.getInstance();
+                myRef = database.getReference("UserInfo").child(uid);
+
+                myRef.child("email").setValue(email);
+                myRef.child("username").setValue(username);
+
+
+                //write userdata into database
+                //writeUserData();
 
                 //make Toast message when input username or password invalid
                 Toast.makeText(RegisterActivity.this, "Register Success", Toast.LENGTH_SHORT).show();
-
+                //back to Login
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                RegisterActivity.this.startActivity(i);
+                finish();
             }
 
         });
@@ -74,6 +102,15 @@ public class RegisterActivity extends AppCompatActivity{
             }
         });
 
+    }//end of onCreate
+
+    public void writeUserData(){
+
+        // Write a message to the database
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message").child(uid).child("text");
+
+        myRef.setValue("Hello, World!");
     }
 
 
